34857b642229e702c5f97b1da61a2420635c2850
Neil Hoff <neilhoff@gmail.com>
2015-05-19 11:01:12 -0500

    Added NYT articles

--- intro_to_ajax/js/script.js
+++ intro_to_ajax/js/script.js
@@ -1,3 +1,6 @@
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 function loadData() {
 
@@ -17,13 +20,31 @@ function loadData() {
     city = $("input#city").val();
     address = street + ", " + city;
 
-    $greeting.text("So you want to live at " + address + "?")
+    $greeting.text("So you want to live at " + address.capitalizeFirstLetter() + "?")
     var bodyImage = "<img class='bgimg' src='https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address + "'>";
 
     // load streetview
     $body.append(bodyImage);
 
-    // YOUR CODE GOES HERE!
+    // load NYT articles
+    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city +"&api-key=" + nytApiKey;
+    $.getJSON(nytUrl, function(data){
+        var articles = [];
+
+        // Get all the NYT documents from the returned JSON
+        var r = data.response.docs;
+
+        // create the list of articles
+        $.each(r, function(key, val){
+           articles.push("<li class='article'><a href='" + val.web_url + "'>" + val.headline.main + "</a><p>" + val.snippet + "</p>");
+        });
+
+        // Add the documents to the unordered list and set the header text
+        $nytElem.html(articles);
+        $nytHeaderElem.text("New York Times Articles about: " + city.capitalizeFirstLetter());
+    }).error(function() {
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+        });
 
     return false;
 };
